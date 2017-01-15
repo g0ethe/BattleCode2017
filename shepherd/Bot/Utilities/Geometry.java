22a3cea3d78a10183c1aec38d4b43cd61e0c5d56
g0ethe <iamgoethe@web.de>
2017-01-15 22:54:40 +0100

    Add files via upload
    Bugfix in lineIntersectsCircle

--- shepherd/Bot/Utilities/Geometry.java
+++ shepherd/Bot/Utilities/Geometry.java
@@ -15,11 +15,11 @@ public class Geometry {
 		}
 		else if(L1x == L2x && L1y != L2y) {
 			float dx = (Cx - L1x < 0) ? (L1x - Cx) : (Cx - L1x);
-			return dx > r;
+			return dx < r;
 		}
 		else if(L1x != L2x && L1y == L2y) {
 			float dy = (Cy - L1y < 0) ? (L1y - Cy) : (Cy - L1y);
-			return dy > r;
+			return dy < r;
 		}
 		else return (L1x - Cx)*(L1x - Cx) + (L1y - Cy)*(L1y - Cy) <= r*r;
 	}
